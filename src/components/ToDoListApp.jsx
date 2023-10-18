2730c625320da7bde4c938351e9e35c471159206
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-18 17:25:20 +0100

    add

--- src/components/ToDoListApp.jsx
+++ src/components/ToDoListApp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {AiOutlineCheck} from 'react-icons/ai'
 import {GiCancel} from 'react-icons/gi'
+import {GrAddCircle} from 'react-icons/gr'
 
 export default function ToDoListApp() {
 
@@ -9,18 +10,28 @@ export default function ToDoListApp() {
         { description: 'Reviser mongodb', completed: false },
         { description: 'Reviser Javascript', completed: true },
     ])
+    const [tache, setTache] = useState('')
+
+    const addTache = () => {
+        setTaches(t => [...t, {description: tache, completed:false}])
+    }
 
     return (
         <div className='todolist'>
-            {taches.map((t, index) => 
-                <div key={index} className='tache'>
-                    <h3>{t.description}</h3>
-                    {
-                        t.completed ? <AiOutlineCheck /> : <GiCancel />
-                    }
-
-                </div>
+            <div className='add'>
+                <input type="text" name="tache" id="tache" placeholder='Saisir une description ...' value={tache} onChange={(e) => setTache(e.target.value)}/>
+                <button onClick={addTache}><GrAddCircle /> Add</button>
+            </div>
+            <div className='taches'>
+                {taches.map((t, index) => 
+                    <div key={index} className='tache'>
+                        <h3>{t.description}</h3>
+                        {
+                            t.completed ? <AiOutlineCheck /> : <GiCancel />
+                        }
+                    </div>
                 )}        
+            </div>
         </div>
     )
 }
